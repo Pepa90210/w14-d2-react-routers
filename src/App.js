08b81dc7bed9861231add4503fb80ff388e5432f
Daniel Hoffmann <103540391+Pepa90210@users.noreply.github.com>
2022-08-09 15:40:10 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
 
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route>
+          <h1>Page Not Found</h1>
+        </Route>
+      </Switch>
+
+      {/* <Home />
       <Stocks />
-      <Movies />
+      <Movies /> */}
     </div>
   );
 }
